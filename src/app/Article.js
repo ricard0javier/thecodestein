f72989ce05bcc79ba64f528c09fe7a522995da04
ricard0javier <villanueva.ricardo@gmail.com>
2016-10-02 06:54:56 +0200

    Article is now been feed by markdown remote content

--- src/app/Article.js
+++ src/app/Article.js
@@ -1,22 +1,22 @@
-class Article extends React.Component {
-
-  constructor() {
-    super();
-    this.state = {content: "Loading..."};
-    this.getContent("static/greetings.md");
-  }
+const Article = React.createClass({
+  getInitialState() {
+    this.getContent(this.props.url);
+    return {content: "Loading..."};
+  },
 
   getContent(url) {
     axios
     .get(url)
     .then(response => {
-      this.setState({content: response.data});
+      this.setState({content: marked(response.data)});
     });
-  }
+  },
 
   render() {
-    return (
-      <div><b>{this.state.content}</b></div>
-      );
+    return <div dangerouslySetInnerHTML={{__html: this.state.content}}/>;
   }
-}
+});
+
+Article.propTypes = {
+  url: React.PropTypes.object.isRequired
+};
