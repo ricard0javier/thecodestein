7498b23ca455a2a6d557b0379ab9e9b92d43ffa6
Ricardo Villanueva <villanueva.ricardo@gmail.com>
2016-10-31 23:19:51 +0000

    styles and scss

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import {Router, Route, browserHistory} from 'react-router';
 
 import {Main} from './app/main';
 
-import '../node_modules/font-awesome/scss/font-awesome.scss';
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import './index.scss';
 
 ReactDOM.render(
